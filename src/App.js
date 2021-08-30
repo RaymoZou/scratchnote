aa916735451e921ad76ba58c0e76d90adbbccdd8
Raymond Zou <raymond.1zou@gmail.com>
2021-08-29 18:52:40 -0700

    add note with button press

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Note from "./Note";
+import NoteArea from "./NoteArea";
+
+let index = 0;
 
 function App() {
+
+    const [notes, setNotes] = useState([])
+
+    function addNote(note) {
+        const { title, body } = note;
+
+        setNotes(prevValue => {
+            return [...prevValue, {
+                key: index++,
+                title: title,
+                body: body,
+            }]
+        })
+    }
+
     return <div>
-        <h1>ScratchNote</h1>
-        <Note title="title" content="a sample note"/>
+        <h1 className="header">ScratchNote</h1>
+        <NoteArea onAdd={addNote} />
+        <Note title="title" content="a sample note" />
+        {notes.map((note) => {
+            console.log(note.body);
+            return <Note title={note.title} content={note.body}></Note>
+        })}
     </div>
 }
 
---
+++ src/NoteArea.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+function NoteArea(props) {
+
+    const [textInput, setTextInput] = useState({
+        title: "",
+        body: ""
+    })
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+
+        setTextInput(prevValue => {
+            return {
+                ...prevValue,
+                [name]: value
+            }
+        })
+    }
+
+    // will only save note if both the title and body are not empty strings
+    function saveNote() {
+        if (textInput.title !== "" && textInput.body !== "") {
+            props.onAdd(textInput);
+        setTextInput(() => {
+            return {
+                title: "",
+                body: ""
+            }
+        })
+        }
+    }
+
+    return (
+        <div>
+            <div className="NoteArea">
+                <input
+                    autoComplete="off"
+                    onChange={handleChange}
+                    name="title" type="text"
+                    placeholder="title"
+                    value={textInput.title}>
+                </input>
+                <input
+                    value={textInput.body}
+                    autoComplete="off"
+                    onChange={handleChange}
+                    name="body" type="text"
+                    placeholder="type your note here..."></input>
+                <button onClick={saveNote}>+</button>
+            </div>
+        </div>
+    )
+}
+
+export default NoteArea;
