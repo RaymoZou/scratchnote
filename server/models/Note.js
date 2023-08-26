ea2135c3539bb66e4e1303d06b00fc0bee2e2b2b
Raymond Zou <raymond.1zou@gmail.com>
2023-08-26 09:32:00 -0700

    add Note schema

---
+++ server/models/Note.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = new mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+})
