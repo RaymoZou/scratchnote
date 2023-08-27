0ad3e7f942178d80c1ece11db82613e5ba69d142
Raymond Zou <raymond.1zou@gmail.com>
2023-08-27 10:18:56 -0700

    add GET endpoint for notes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,29 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express()
 const mongoose = require('mongoose');
+const Note = require('./models/Note');
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI);
 
 app.get('/', (req, res) => {
     res.send('hello world');
+})
+
+app.post('/addNote', async (req, res) => {
+    const note = req.body;
+    const newNote = new Note(note);
+    await newNote.save();
+    res.json(newNote);
+})
+
+app.get('/getNotes', async (req, res) => {
+    const notes = await Note.find();
+    res.send(notes);
 })
 
 app.listen(process.env.PORT, () => console.log('Listening on port ' + process.env.PORT))
