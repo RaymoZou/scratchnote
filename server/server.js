60d3c5da08e9038216f3229ce7cfd93b35f1970b
Raymond Zou <raymond.1zou@gmail.com>
2023-10-24 21:03:13 -0700

    code consistency

--- server/server.js
+++ server/server.js
@@ -12,29 +12,28 @@ mongoose.connect(process.env.MONGODB_URI);
 
 app.get('/', (req, res) => {
     res.send('hello world');
-})
+});
 
 app.post('/addNote', async (req, res) => {
     const note = req.body;
     const newNote = new Note(note);
     await newNote.save();
     res.json(newNote);
-})
+});
 
 app.get('/getNotes', async (req, res) => {
     const notes = await Note.find();
     res.send(notes);
-})
+});
 
 app.delete('/deleteNote/:id', async (req, res) => {
     try {
         const id = req.params.id;
-        await Note.deleteOne({_id: id})
-        
+        await Note.deleteOne({ _id: id });
         res.send(`note with id ${id} successfully deleted`); // added this line
     } catch (err) {
         console.log('there was an error deleting the post');
     }
-})
+});
 
-app.listen(process.env.PORT, () => console.log('Listening on port ' + process.env.PORT))
+app.listen(process.env.PORT, () => console.log('Listening on port ' + process.env.PORT));
